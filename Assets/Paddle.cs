b2965ce86dadc2b0f47136e3f8860d9239fd9b9e
Roberto Breve <rbreve@gmail.com>
2019-11-16 10:33:48 +0200

    old project deleted, new project

---
+++ Assets/Paddle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 position = this.transform.position;
+
+        if(position.z > -2 && speed < 0)
+        {
+            position.z += speed;
+            this.transform.position = position;
+         
+
+        }
+
+        if (position.z < 10 && speed > 0) {
+            position.z += speed;
+            this.transform.position = position;
+        }
+
+
+ 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            speed = 0.4f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            speed = -0.4f;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //print(collision.gameObject.name);
+        //Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        //rb.AddForce(Vector3.left * 500); ;
+
+    }
+
+}
---
+++ Assets/ball.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // rb.AddForce(transform.forward * 600);
+        rb.AddForce(new Vector3(200, 0, 600));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "zone")
+        {
+            rb.position = new Vector3(11,0,2);
+          // rb.AddForce(new Vector3(Random.Range(100, 100), 0, Random.Range(100, 100)));
+
+        }
+        //print(collision.gameObject.name);
+        //Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        //rb.AddForce(Vector3.left * 500); ;
+
+    }
+}
