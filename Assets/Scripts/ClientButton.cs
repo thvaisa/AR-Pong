d7905b3436d607c013badb0c34f0dfbba05d4d71
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-11-16 17:51:43 +0200

    Fix references and add disconnect feature

--- Assets/Scripts/ClientButton.cs
+++ Assets/Scripts/ClientButton.cs
@@ -12,22 +12,35 @@ public class ClientButton : MonoBehaviour {
     [SerializeField]
     private TMP_InputField ipField;
     private Button button;
+    private TextMeshProUGUI text;
 
     private void Awake() {
         button = GetComponent<Button>();
+        text = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     private void OnEnable() {
-        button.onClick.AddListener(OnClickHandler);
+        button.onClick.AddListener(Connect);
     }
 
     private void OnDisable() {
         button.onClick.RemoveAllListeners();
     }
 
-    public void OnClickHandler() {
+    private void Connect() {
         manager.networkAddress = ipField.text;
         manager.StartClient();
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(Disconnect);
+        text.text = "Disconnect";
         status.text = "Connecting...";
+    }
+
+    private void Disconnect() {
+        manager.StopClient();
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(Connect);
+        text.text = "Connect";
+        status.text = "Not connected";
     }
 }
